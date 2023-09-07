7e888c430fc54b0d6038c33a75183453617690fa
venkat2602 <19h61a05j4@cvsr.ac.in>
2023-09-07 18:15:30 +0530

    Q-estate-Backend

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
+
+const port = 5000;
+
+server.use(middleware);
+server.use(router);
+
+server.listen(port);
